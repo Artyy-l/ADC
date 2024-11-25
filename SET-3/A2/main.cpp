9286c8523ce3a77bcfd6d10fb0541a2803852311
Валерия Чапурина <140277274+Artyy-l@users.noreply.github.com>
2024-11-25 21:38:21 +0300

    Update main.cpp

--- SET-3/A2/main.cpp
+++ SET-3/A2/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <fstream>
 #include <algorithm>
+#include <string>
 
 class ArrayGenerator {
+private:
+    int minValue, maxValue;
+    std::mt19937 rd;
 public:
     ArrayGenerator(int minValue = 0, int maxValue = 6000)
         : minValue(minValue), maxValue(maxValue), rd(std::random_device{}()) {}
@@ -19,11 +24,11 @@ public:
 
     std::vector<int> generateSortedReverseArray(int size) {
         std::vector<int> arr = generateRandomArray(size);
-        std::sort(arr.begin(), arr.end(), std::greater<int>());
+        std::sort(arr.rbegin(), arr.rend());
         return arr;
     }
 
-    std::vector<int> generateNearlySortedArray(int size, int swapCount = 10) {
+    std::vector<int> generateAlmostSortedArray(int size, int swapCount = 10) {
         std::vector<int> arr = generateRandomArray(size);
         std::sort(arr.begin(), arr.end());
 
@@ -35,20 +40,86 @@ public:
         }
         return arr;
     }
+};
 
-    std::vector<int> generateSubarray(int size) {
-        std::vector<int> fullArray = generateRandomArray(10000);
-        return std::vector<int>(fullArray.begin(), fullArray.begin() + size);
+void PrintArray(std::vector<int>& arr) {
+    for (auto& elem : arr) {
+        std::cout << elem << " ";
+    }
+    std::cout << "\n";
+}
+
+template<typename T>
+void PrintArrayIntoFile(std::ofstream &file, const std::vector<T> &array) {
+    for (size_t i = 0; i < array.size(); ++i) {
+        file << array[i];
+        if (i < array.size() - 1) {
+            file << ", ";
+        }
+    }
+    file << '\n';
+}
+
+void createTestFileFromBigArray(std::vector<int> arr, int maxSize, const char* fileName) {
+    std::string fullFileName = std::string("../A2_") + fileName + ".txt";
+    std::ofstream outFile(fullFileName);
+    for (int size = 500; size < maxSize; size += 100) {
+        std::vector<int> curArray(arr.begin(), arr.begin() + size);
+        PrintArrayIntoFile(outFile, curArray);
+    }
+    outFile.close();
+}
+
+void createTestFile(
+        std::vector<int> (ArrayGenerator::*generatorFunction)(int),
+        ArrayGenerator& generator,
+        int maxSize,
+        const char* fileName) {
+
+    std::string fullFileName = std::string("../A2_") + fileName + ".txt";
+    std::ofstream outFile(fullFileName);
+
+    for (int size = 500; size < maxSize; size += 100) {
+        std::vector<int> curArray = (generator.*generatorFunction)(size);
+        PrintArrayIntoFile(outFile, curArray);
     }
 
-private:
-    int minValue, maxValue;
-    std::mt19937 rd;
-};
+    outFile.close();
+}
+
+void createTestFileForAlmostSortedArray(ArrayGenerator& generator, int maxSize, const char* fileName) {
+
+    std::string fullFileName = std::string("../A2_") + fileName + ".txt";
+    std::ofstream outFile(fullFileName);
+
+    for (int size = 500; size < maxSize; size += 100) {
+        std::vector<int> curArray = generator.generateAlmostSortedArray(size, size / 50);
+        PrintArrayIntoFile(outFile, curArray);
+    }
+
+    outFile.close();
+}
 
 int main() {
     ArrayGenerator generator;
+    const int maxSize = 10000;
+    std::vector<int> randomArray = generator.generateRandomArray(maxSize);
+    std::vector<int> sortedReversedArray = generator.generateSortedReverseArray(maxSize);
+    std::vector<int> almostSortedArray = generator.generateAlmostSortedArray(maxSize, 100);
 
+//    PrintArray(randomArray);
+//    PrintArray(sortedReversedArray);
+//    PrintArray(almostSortedArray);
+
+    // массивы создаём из заранее заготовленного большого массива
+    createTestFileFromBigArray(randomArray, maxSize, "RandomArraysFromBigArray");
+    createTestFileFromBigArray(sortedReversedArray, maxSize, "SortedReversedArraysFromBigArray");
+    createTestFileFromBigArray(almostSortedArray, maxSize, "AlmostSortedArraysFromBigArray");
+    
+    // массивы создаём заново каждый раз
+    createTestFile(&ArrayGenerator::generateRandomArray, generator, maxSize, "RandomArrays");
+    createTestFile(&ArrayGenerator::generateSortedReverseArray, generator, maxSize, "SortedReversedArrays");
+    createTestFileForAlmostSortedArray(generator, maxSize, "AlmostSortedArrays");
 
     return 0;
 }
